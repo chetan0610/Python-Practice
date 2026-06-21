55a36ee6ac5fbb0ca7e6b5672895c2a154f9bc34
Chetan0806 <chetanmutyala@gmail.com>
2026-06-21 12:15:39 +0530

    Fix calculator logic

--- Day01/my_own.py
+++ Day01/my_own.py
@@ -1,14 +1,6 @@
 Number_of_Items = int(input("Enter number of items: "))
 Items = input("Enter the Items: ")
-for i  in range(1,Number_of_Items + 1, Items):
 
-Quantity = int(input("Enter the quantity: "))
-Price = int(input("Enter the Price of the item: "))
-GST = int(input("Enter the GST of the price: "))
-
-
-Sub_Total = Price*Quantity
-GST_Amount = int(Sub_Total*GST)/100
-FInal_Bill = Sub_Total+GST_Amount
-
-    
+print()
+print("Enter number of Items: ",Number_of_Items)
+print("Enter the name of the Item: ",Items)
